868c9720f6b51832851899d790f708dd0f24dc8e
amberllinden <amberlinden@icloud.com>
2019-08-12 17:36:38 +0300

    Обновила gitignore, создала основу проекта, настроила сборщик

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app/components/App.js";
+import './app/styles/App.css';
+
+ReactDOM.render(<App />, document.getElementById("root")); 
