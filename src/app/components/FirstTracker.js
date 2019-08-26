e6ef92bba150e9eea651c6403ff041bc01646da2
amberllinden <amberlinden@icloud.com>
2019-08-26 17:18:26 +0300

    Добавила возможность менять шрифт в текстовом блоке

--- src/app/components/FirstTracker.js
+++ src/app/components/FirstTracker.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { FirstTrackerTitleBlock } from './elements/FirstTrackerTitleBlock'
 import { FirstTrackerHabbitsblockForStrings } from "./elements/FirstTrackerHabbitsblockForStrings";
 import { FirstTrackerStringWithDays } from "./elements/FirstTrackerStringWithDays";
+import { data_obj } from "./data/styles";
 
 
 export class FirstTracker extends Component
@@ -12,11 +13,13 @@ export class FirstTracker extends Component
         this.state = {
             title_text: '',
             month_text: '',
-            qoute_text: ''
+            qoute_text: '',
+            font_value: ''
         }
         this.changeTitle = this.changeTitle.bind(this);
         this.changeMonth = this.changeMonth.bind(this);
         this.changeQoute = this.changeQoute.bind(this);
+        this.changeFont = this.changeFont.bind(this);
     }
 
     changeTitle(event) {
@@ -28,21 +31,30 @@ export class FirstTracker extends Component
     changeQoute(event) {
         this.setState({ qoute_text: event.target.value })
     }
-    
+    changeFont(event) {
+        event.target.value == 'Выберите шрифт' ? this.setState({ font_value: '' }) : this.setState({ font_value: event.target.value })
+    }
 
     render() {
-        let { title_text, month_text, qoute_text } = this.state;
+        let { title_text, month_text, qoute_text, font_value } = this.state;
+        const fonts = data_obj.fonts;
+
         return (
             <section className="first_tracker">
                 <div className="first_tracker--constructor">
-                    <input type="text" value={this.state.title_text} onChange={this.changeTitle} placeholder="Название трекера"/>
-                    <input type="text" value={this.state.month_text} onChange={this.changeMonth} placeholder="Месяц трекера" />
-                    <input type="text" value={this.state.qoute_text} onChange={this.changeQoute} placeholder="Мотивационная цитата" />
+                    <input type="text" value={this.state.title_text} onChange={this.changeTitle} maxLength={145} placeholder="Название трекера"/>
+                    <input type="text" value={this.state.month_text} onChange={this.changeMonth} maxLength={145} placeholder="Месяц трекера" />
+                    <input type="text" value={this.state.qoute_text} onChange={this.changeQoute} maxLength={145} placeholder="Мотивационная цитата" />
+                    <select name="font_select" value={font_value} onChange={this.changeFont}>
+                        {
+                            fonts.map((font, index) => <option key={index} value={font.name}>{font.name}</option>)
+                        }
+                    </select>
                 </div>
                 <FirstTrackerTitleBlock
-                    title_block={'first_tracker--title_block--title'}
-                    month_block={'first_tracker--title_block--month'}
-                    qoute_block={'first_tracker--title_block--quote'}
+                    title_block={`first_tracker--title_block--title ${font_value}`}
+                    month_block={`first_tracker--title_block--month ${font_value}`}
+                    qoute_block={`first_tracker--title_block--quote ${font_value}`}
                     span_class={'first_tracker--title'}
                     title_text={title_text}
                     month_text={month_text}
---
+++ src/app/components/data/styles.js
@@ -0,0 +1,15 @@
+export  const data_obj = {
+    fonts : [
+        {name: 'Выберите шрифт'},
+        {name: 'opensans'},
+        {name: 'condensed'},
+        {name: 'lobster'},
+        {name: 'pacifico'},
+        {name: 'amatic'},
+        {name: 'caveat'},
+        {name: 'rubik'},
+        {name: 'kurale'},
+        {name: 'underdog'},
+        {name: 'marck'}
+    ]
+}
