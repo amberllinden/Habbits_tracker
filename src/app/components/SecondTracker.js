ea6d7aa47b4b106c6dfa4caf0cd0f4b389268dfd
amberllinden <amberlinden@icloud.com>
2019-09-03 17:44:07 +0300

    Начала реализацию второго трекера

---
+++ src/app/components/SecondTracker.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { data_obj } from "./data/styles";
+
+export class SecondTracker extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <div></div>
+        )
+    }
+}
