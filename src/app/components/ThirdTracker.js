3be798934e1bebd5c615194af673d539addf5b40
amberllinden <amberlinden@icloud.com>
2019-09-13 14:10:27 +0300

    Начала работу над третьим вариантом треккера

---
+++ src/app/components/ThirdTracker.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { data_obj } from "./data/styles";
+
+export class ThirdTracker extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <section className="third_tracker">
+
+            </section>
+        )
+    }
+}
