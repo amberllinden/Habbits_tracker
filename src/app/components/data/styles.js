c3196d49ea35161e188a0ea81d8af829a9528029
amberllinden <amberlinden@icloud.com>
2019-09-16 22:19:48 +0300

    Добавила стили для третьего треккера, реализовала смену стилей

--- src/app/components/data/styles.js
+++ src/app/components/data/styles.js
@@ -1,16 +1,16 @@
 export  const data_obj = {
     fonts : [
-        {name: 'Выберите шрифт'},
-        {name: 'opensans'},
-        {name: 'condensed'},
-        {name: 'lobster'},
-        {name: 'pacifico'},
-        {name: 'amatic'},
-        {name: 'caveat'},
-        {name: 'rubik'},
-        {name: 'kurale'},
-        {name: 'underdog'},
-        {name: 'marck'}
+        {name: "Выберите шрифт"},
+        {name: "opensans"},
+        {name: "condensed"},
+        {name: "lobster"},
+        {name: "pacifico"},
+        {name: "amatic"},
+        {name: "caveat"},
+        {name: "rubik"},
+        {name: "kurale"},
+        {name: "underdog"},
+        {name: "marc"}
     ],
     colors_fonts : [
         { name: "Выберите цвет",
@@ -81,5 +81,106 @@ export  const data_obj = {
         name: "Saddlebrown",
         value: "saddlebrown-border"
       },
+    ],
+    color_scheme: [
+      {
+        name: "Выберите цветовую схему",
+        value: "0",
+        options: {
+          color_day: "",
+          color_span: "",
+          color_habbit: "",
+        }
+      },
+      {
+        name: "1",
+        value: "1",
+        options: {
+          color_day: "chernika-day",
+          color_span: "chernika",
+          color_habbit: "chernika-hab",
+        }
+      },
+      {
+        name: "2",
+        value: "2",
+        options: {
+          color_day: "flower-day",
+          color_span: "flower",
+          color_habbit: "flower-hab",
+        }
+      },
+      {
+        name: "3",
+        value: "3",
+        options: { 
+          color_day: "green_flower-day",
+          color_span: "green_flower",
+          color_habbit: "green_flower-hab",
+        }
+      },
+      {
+        name: "4",
+        value: "4",
+        options: {
+          color_day: "sukkulent-day",
+          color_span: "sukkulent",
+          color_habbit: "sukkulent-hab",
+        }
+      },
+      {
+        name: "5",
+        value: "5",
+          options: {
+          color_day: "lilac-day",
+          color_span: "lilac",
+          color_habbit: "lilac-hab",
+        }
+      },
+      {
+        name: "6",
+        value: "6",
+        options: {
+          color_day: "cheese-day",
+          color_span: "cheese",
+          color_habbit: "cheese-hab",
+        }
+      },
+      {
+        name: "7",
+        value: "7",
+        options: {
+          color_day: "fog-day",
+          color_span: "fog",
+          color_habbit: "fog-hab",
+        }
+      },
+      {
+        name: "8",
+        value: "8",
+        options: {
+          color_day: "sea_beach-day",
+          color_span: "sea_beach",
+          color_habbit: "sea_beach-hab",
+        }
+      },
+      {
+        name: "9",
+        value: "9",
+        options: {
+          color_day: "coffee-day",
+          color_span: "coffee",
+          color_habbit: "coffee-hab",
+        }
+      },
+      {
+        name: "10",
+        value: "10",
+        options: {
+          color_day: "chocolate-day",
+          color_span: "chocolate",
+          color_habbit: "chocolate-hab",
+        }
+      }
     ]
-}
+  }
