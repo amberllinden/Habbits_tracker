3bce509490067eaf7bb5f91c72b3d46ed6c5df43
amberllinden <amberlinden@icloud.com>
2019-09-17 15:46:33 +0300

    Добавила роутинг, внесла правки в CSS

---
+++ src/app/pages/FirstTracker.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { FirstTracker } from "../components/FirstTracker";
+
+export class First extends Component {
+    render() {
+        return (
+            <FirstTracker />
+        )
+    }
+}
---
+++ src/app/pages/SecondTracker.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { SecondTracker } from "../components/SecondTracker";
+
+export class Second extends Component {
+    render() {
+        return (
+            <SecondTracker />
+        )
+    }
+}
