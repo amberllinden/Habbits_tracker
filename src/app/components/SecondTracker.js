ba6e84a33fe46548e0715764f2c94d054c659c74
amberllinden <amberlinden@icloud.com>
2019-10-21 14:34:57 +0300

    Начата работа над треккером, обновлен файл с данными для классов

--- src/app/components/SecondTracker.js
+++ src/app/components/SecondTracker.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { data_obj } from "./data/styles";
 import { Select } from "./elements/Select";
+import { Input } from "./elements/Input";
 
 export class SecondTracker extends Component {
     constructor(props) {
@@ -14,10 +15,70 @@ export class SecondTracker extends Component {
 
     render() {
         
-        const { } = data_obj; 
+        const { days_of_week_rus, days_of_week_en, icons } = data_obj; 
 
         return (
-            <div></div>
+            <section className="fourth_tracker">
+                <div className="tracker_select second_tracker-selects">
+                    <div className="tracker_select_column">
+                        <Select
+                            value={}
+                            arr={}
+                            css_class="second_tracker_select"
+                            changeStyle={}
+                        />
+                        <Select
+                            value={}
+                            arr={}
+                            css_class="second_tracker_select"
+                            changeStyle={}
+                        />
+                        <Select
+                            value={}
+                            arr={}
+                            css_class="second_tracker_select"
+                            changeStyle={}
+                        />
+                        <Select
+                            value={}
+                            arr={}
+                            css_class=""
+                            changeStyle={}
+                        />
+                        <Select
+                            value={}
+                            arr={}
+                            css_class="second_tracker_select"
+                            changeStyle={}
+                        />
+                        {
+                        // Появляется в зависимости от значения чекбокса
+                        <Select
+                            value={}
+                            arr={}
+                            css_class="second_tracker_select"
+                            changeStyle={}
+                        />
+                        }
+                    </div>
+                    <div className="tracker_select_column">
+                        <Input
+                            wrapper_css="label_input"
+                            input_name="days"
+                            input_type="checkbox"
+                            label="Дни недели на английском"
+                            changeStyle={}
+                        />
+                        <Input
+                            wrapper_css="label_input"
+                            input_name="icons"
+                            input_type="checkbox"
+                            label="Значки у типов треккеров"
+                            changeStyle={}
+                        />
+                    </div>
+                </div>
+            </section>
         )
     }
 }
